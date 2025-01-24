2f2bf00449322723992c392f7c5ee8b7dd375dd2
Priyam-SahaGit31200 <priyam312006@gmail.com>
2025-01-24 14:44:38 +0530

    Update Armstrong number using While.c

--- Day-3/Armstrong number using While.c
+++ Day-3/Armstrong number using While.c
@@ -2,35 +2,31 @@
 #include <math.h>
 
 int main() {
-    int num, originalNum,remainder,n=0,sum=0;
+    int num, originalNum, remainder, n = 0, result = 0;
+
 
     printf("Enter an integer: ");
-    scanf("%d",&num);
+    scanf("%d", &num);
 
-    originalNum=num;
-
-    while (originalNum!= 0) {
-        originalNum/=10;
-        ++n;
+    originalNum = num; 
+   
+    while (originalNum != 0) {
+        originalNum /= 10;
+        n++;
     }
 
-    originalNumnum;
+    originalNum = num; 
 
-    while(originalNum!= 0) 
-	{
-        remainder=originalNum%10;
-        sum += pow(remainder, n);
-        originalNum/=10;
+    while (originalNum != 0) {
+        remainder = originalNum % 10; 
+        result += pow(remainder, n); 
+        originalNum /= 10; 
     }
 
-    if(sum==num) 
-	{
-        printf("%d is an Armstrong number.\n",num);
-    } 
-    
-	else 
-	{
-        printf("%d is not an Armstrong number.\n",num);
+    if (result == num) {
+        printf("%d is an Armstrong number.\n", num);
+    } else {
+        printf("%d is not an Armstrong number.\n", num);
     }
 
     return 0;
